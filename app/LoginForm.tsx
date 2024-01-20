36514e2df58dc06681762d460bfa1f965abc0124
Ahmad Nabelsi <ahmad@wearecws.com>
2024-01-20 15:44:27 -0500

    quick forms

---
+++ app/LoginForm.tsx
@@ -0,0 +1,16 @@
+// components/LoginForm.js
+const LoginForm = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white shadow-lg">
+                <h1 className="text-2xl font-bold text-center">Login</h1>
+                <form className="space-y-6" action="#">
+                    <input type="email" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1" placeholder="Email" />
+                    <input type="password" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1" placeholder="Password" />
+                    <button type="submit" className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700">Login</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+export default LoginForm;
---
+++ app/SignUpForm.tsx
@@ -0,0 +1,17 @@
+// components/SignupForm.js
+const SignupForm = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-white shadow-lg">
+                <h1 className="text-2xl font-bold text-center">Sign Up</h1>
+                <form className="space-y-6" action="#">
+                    <input type="text" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1" placeholder="Full Name" />
+                    <input type="email" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1" placeholder="Email" />
+                    <input type="password" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-1" placeholder="Password" />
+                    <button type="submit" className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700">Sign Up</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+export default SignupForm;
